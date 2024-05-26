2dbe601aff30b5ce34e7a8afdde6af6d13af2cf5
billionercat-dev <142187629+billionercat-dev@users.noreply.github.com>
2024-05-26 20:44:27 +0700

    Add files via upload

---
+++ HTML2/aihoi.js
@@ -0,0 +1,13 @@
+rick = document.getElementById("rickroll");
+
+function nggyu() {
+    rick.style.opacity = 1;
+    rick.width="956";
+    rick.height="538";
+    rick.src="https://www.youtube.com/embed/dQw4w9WgXcQ";
+    rick.title="Rick Astley - Never Gonna Give You Up (Official Music Video)";
+    rick.frameborder="0";
+    rick.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"; 
+    rick.referrerpolicy="strict-origin-when-cross-origin";
+    rick.allowfullscreen;
+}
