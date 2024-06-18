82a709e23916d8c5f69e0d3d313ddf97e93b1f50
billionercat-dev <142187629+billionercat-dev@users.noreply.github.com>
2024-06-18 16:21:29 +0700

    Update aihoi.js

--- HTML2/aihoi.js
+++ HTML2/aihoi.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 rick = document.getElementById("rickroll");
 
 function nggyu() {
@@ -12,18 +12,3 @@ function nggyu() {
     rick.referrerpolicy="strict-origin-when-cross-origin";
     rick.allowfullscreen;
 }
-=======
-rick = document.getElementById("rickroll");
-
-function nggyu() {
-    rick.style.opacity = 1;
-    rick.width="956";
-    rick.height="538";
-    rick.src="https://www.youtube.com/embed/dQw4w9WgXcQ";
-    rick.title="Rick Astley - Never Gonna Give You Up (Official Music Video)";
-    rick.frameborder="0";
-    rick.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"; 
-    rick.referrerpolicy="strict-origin-when-cross-origin";
-    rick.allowfullscreen;
-}
->>>>>>> 521a4f0a5b9c6a3a42e94300a7ba10ac2f598f2a
